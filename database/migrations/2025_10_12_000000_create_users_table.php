e489c3e8c0b82a2b5ac756ad361b4129aec24792
Abdul Rozzaq <abdrozzaq02@gmail.com>
2024-12-03 16:56:11 +0900

    fix: token not created

--- database/migrations/2025_10_12_000000_create_users_table.php
+++ database/migrations/2025_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('id_user');
+            $table->id();
             $table->integer('id_jabatan')->nullable()->unsigned();
             $table->integer('id_atasan')->nullable()->unsigned();
             $table->string('nama');
